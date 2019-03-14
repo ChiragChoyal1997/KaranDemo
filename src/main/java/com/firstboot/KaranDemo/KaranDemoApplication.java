1425921026c673d39d2625c6f7bcfd84496fe86a
Chirag Choyal <chiragchoyal@gmail.com>
2019-03-15 00:01:18 +0530

    first commit

---
+++ src/main/java/com/firstboot/KaranDemo/KaranDemoApplication.java
@@ -0,0 +1,14 @@
+package com.firstboot.KaranDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaranDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KaranDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/firstboot/KaranDemo/controller/FirstController.java
@@ -0,0 +1,13 @@
+package com.firstboot.KaranDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FirstController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
